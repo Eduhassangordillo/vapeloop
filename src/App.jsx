9da19e0498a0abd63ef8e0733f6dec84adcf0cf9
Eduhassangordillo <hassangordillo.eduardo@gmail.com>
2024-04-30 19:17:23 -0300

    add product detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoutes from "./routes/ProtectRoutes";
 
 import Home_Page from "./pages/Home_Page";
-import Product_Page from "./pages/Product_Page";
 import Admin_Page from "./pages/Admin_Page";
+import Product_Page from "./pages/Product_Page";
+import ProductDetailPage from "./components/ProductDetailPage";
 import Nav_bar from "./components/Nav_bar";
-
 import "./css/App.css";
-import { useState } from "react";
+
+// Importa los archivos JSON de productos
+import descartablesData from "./json/descartablesData.json";
+import podsData from "./json/podsData.json";
 
 function App() {
   const [auth, setAuth] = useState(false);
@@ -15,6 +19,7 @@ function App() {
   const logIn = () => {
     setAuth(true);
   };
+
   const logOut = () => {
     setAuth(false);
   };
@@ -26,7 +31,21 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home_Page />} />
-        <Route path="product" element={<Product_Page />} />
+        <Route
+          path="/product"
+          element={
+            <Product_Page descartables={descartablesData} pods={podsData} />
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <ProductDetailPage
+              descartables={descartablesData}
+              pods={podsData}
+            />
+          }
+        />
         <Route
           path="/admin"
           element={
--- src/components/CardsProduct.jsx
+++ src/components/CardsProduct.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 
 const CardsProduct = ({ image, productName }) => {
   return (
     <div className="p-2">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{productName}</Card.Title>
-          <Card.Text>
+      <div className="card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={image} alt={productName} />
+        <div className="card-body">
+          <h5 className="card-title">{productName}</h5>
+          <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Ver más</Button>
-        </Card.Body>
-      </Card>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
